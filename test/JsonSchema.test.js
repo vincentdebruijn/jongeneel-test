f5840935cab9926d4eaa397e27919fe813aa44f0
Vincent de Bruijn <dbruijn@axini.com>
2017-09-07 13:15:28 +0200

    wip

--- test/JsonSchema.test.js
+++ test/JsonSchema.test.js
@@ -1,25 +1,44 @@
-const assert = require('assert');
+const assert = require('chai').assert;
 const fetch = require('node-fetch');
+const Validator = require('jsonschema').Validator;
+
+const validator = new Validator();
+const schema = require('../api/api.json');
 const host = process.env.HOST;
 
 describe('JSON schemas', () => {
-  // TODO: parse JSON schema
-  // const schema = ...;
-  const tests = [{ href: `/products/ZH0670`, schema: '/products/' }];
+  // Assuming all tests are for the responses to GET
+  const tests = [{ href: `/products/ZH0670`, path: '/products/{id}' }];
+  const getJson = function(res) {
+    return res.text();
+  };
 
   tests.forEach(function(test) {
     it(`gives a valid response for ${test.href}`, function(done) {
       // TODO: use url helper
-      fetch(`${host}${test.href}`)
-        .then(res => {
-          return res.text();
-        })
-        .then(json => {
+      fetch(`${host}${test.href}`).then(res => {
+        let status = res.status;
+        getJson(res).then(json => {
           console.log(json);
-          // TODO: get and assert the schema
-          assert(true);
+          // Add the path and status code to the response
+          // let responseSchema = {};
+          // responseSchema[status] = { schema: json };
+          // let pathSchema = {};
+          // pathSchema[test.schema] = { get: { responses: responseSchema } };
+          // // Validate the response
+          // //TODO: always passes...
+          // let result = validator.validate({ paths: pathSchema }, schema);
+
+          let responseSchema = schema.paths[test.path].get.responses[status].schema;
+          let validateSchema = { definitions: schema.definitions };
+          Object.assign(validateSchema, responseSchema);
+          let result = validator.validate(json, validateSchema);
+
+          console.log(result);
+          assert.isEmpty(result.errors);
           done();
         });
+      });
     });
   });
 });
